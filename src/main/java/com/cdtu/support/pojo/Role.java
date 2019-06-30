d5fca9e37da2fab024585ff956206ac6d3ab1eec
wycyz1 <783744175@qq.com>
2019-06-30 21:24:54 +0800

    6 30 21：22吴洋

---
+++ src/main/java/com/cdtu/support/pojo/Role.java
@@ -0,0 +1,43 @@
+package com.cdtu.support.pojo;
+
+public class Role {
+    private Integer id;
+
+    private String name;
+
+    private String info;
+
+    private String other;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info == null ? null : info.trim();
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other == null ? null : other.trim();
+    }
+}
